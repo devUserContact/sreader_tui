d44c8afb8b7a3cc4bca93a8696ec4483e75cf5c0
devUserContact <developer.user.contact@protonmail.com>
2023-10-23 22:24:34 -0700

    included draft `load_book` function

--- src/sreader/iter_text.rs
+++ src/sreader/iter_text.rs
@@ -1,4 +1,20 @@
+use std::fs;
+
 #[derive(Debug)]
-pub struct IterText {
-    
+pub struct IterText {}
+
+struct BookSlice {
+    array: Vec<String>,
+    length: usize,
+}
+
+fn load_book() {
+    let book: String =
+        fs::read_to_string("assets/book/lewisCarroll_alicesAdventuresInWonderland.txt")
+            .expect("failed to read file");
+    let book_words: Vec<String> = book.split(" ").map(|s| s.to_string()).collect();
+    BookSlice {
+        array: book_words.clone(),
+        length: book_words.len(),
+    };
 }
